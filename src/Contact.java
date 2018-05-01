44aaa91ac48a4fe2149cbfe0ac5dbfe20474dc9e
gyun-notebook <chl8263@naver.com>
2018-05-01 21:45:19 +0900

    asd

--- src/Contact.java
+++ src/Contact.java
@@ -2,5 +2,4 @@ import java.util.ArrayList;
 
 public class Contact {
     static ArrayList<String> inputString = new ArrayList<String>();
-    static ArrayList<String> inputString2 = new ArrayList<String>();
 }
